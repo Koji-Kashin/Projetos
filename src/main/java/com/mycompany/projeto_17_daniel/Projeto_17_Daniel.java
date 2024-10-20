255f234c64894f88489e76c57dd7019b61584595
Koji-Kashin <daniel.f.rodrigues6@aluno.senai.br>
2024-10-19 22:57:28 -0300

    Projeto 17

---
+++ src/main/java/com/mycompany/projeto_17_daniel/Projeto_17_Daniel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_17_daniel;
+
+/**
+ *
+ * @author The Great Garibaldo
+ */
+public class Projeto_17_Daniel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/projeto_17_daniel/statement.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projeto_17_daniel;
+
+/**
+ *
+ * @author The Great Garibaldo
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+    
+}
